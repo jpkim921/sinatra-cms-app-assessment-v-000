bd0a00fe6334604106db0e327a083a94680573fb
Jayon Kim <jpkim921@gmail.com>
2018-06-17 20:50:54 +0000

    automatically backed up by learn

--- db/migrate/20180617205033_create_activities_table.rb
+++ db/migrate/20180617205033_create_activities_table.rb
@@ -1,4 +1,15 @@
 class CreateActivitiesTable < ActiveRecord::Migration
   def change
+    create_table :activities do |t|
+      t.string :name
+      t.string :activity_date
+      t.integer :activity_time
+      t.float :distance
+      t.float :pace_avg
+      t.float :speed_avg
+      t.float :hr_avg
+      t.integer :user_id
+      t.integer :category_id
+    end
   end
 end
