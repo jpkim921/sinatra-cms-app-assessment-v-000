6e0f8bf4dff2023734489269be53c98f158fe15c
Jayon Kim <jpkim921@gmail.com>
2018-06-17 20:50:02 +0000

    automatically backed up by learn

--- db/migrate/20180617204920_create_users_table.rb
+++ db/migrate/20180617204920_create_users_table.rb
@@ -1,4 +1,24 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :dob
+      t.string :weight
+    end
+  end
+end
+
+
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :dob
+      t.string :weight
+    end
   end
 end
